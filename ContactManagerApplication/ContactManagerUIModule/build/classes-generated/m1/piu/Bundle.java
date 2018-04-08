2bdeb0f96577d8ea226639ee2f862ffcafe51d33
fLaVz <juliendelvaux@neuf.fr>
2018-04-08 11:30:25 +0200

    initial commit

---
+++ ContactManagerApplication/ContactManagerUIModule/build/classes-generated/m1/piu/Bundle.java
@@ -0,0 +1,13 @@
+package m1.piu;
+/** Localizable strings for {@link m1.piu}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Clear</i>
+     * @see Clear
+     */
+    static String CTL_Clear() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_Clear");
+    }
+    private Bundle() {}
+}
